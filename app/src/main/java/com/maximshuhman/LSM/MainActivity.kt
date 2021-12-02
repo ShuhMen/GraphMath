18fa7f8e1a5abbbe152653fe458a19fc2f844934
ShuhMen <59605174+ShuhMen@users.noreply.github.com>
2021-12-02 22:52:53 +0300

    Добавлен toolbar, добавлена кнопка, вызывающая новую активность, где в будущем будут настройки темы. добавлена розовая тема

--- app/src/main/java/com/maximshuhman/LSM/MainActivity.kt
+++ app/src/main/java/com/maximshuhman/LSM/MainActivity.kt
@@ -1,14 +1,13 @@
 package com.maximshuhman.LSM
 
+import android.content.Intent
 import android.content.res.ColorStateList
 import android.os.Bundle
-import android.view.Gravity
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -21,12 +20,9 @@ class MainActivity : AppCompatActivity() {
     lateinit var AddValue: Button
     lateinit var DelButton: Button
     lateinit var EmptyText: TextView
-
+    lateinit var toolBar: Toolbar
 
     var mode: Boolean = false
-
-
-
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
@@ -38,13 +34,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        Utils.onActivityCreateSetTheme(this);
 
         ValuesRecyclerView = findViewById(R.id.val_recycler_view)
         CalcButton = findViewById(R.id.calc_button)
         AddValue = findViewById(R.id.add_value)
         DelButton = findViewById(R.id.del_button)
-
-
 
         ValuesRecyclerView.layoutManager = LinearLayoutManager(this)
 
@@ -134,6 +131,28 @@ class MainActivity : AppCompatActivity() {
 
         updateUI(ValueList.data)
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+
+        menuInflater.inflate(R.menu.app_bar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.settings-> {
+
+
+                setTheme(R.style.Pink);
+                val intent = Intent(this, SettingsActivity::class.java)
+                intent.flags =
+                    Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+                startActivity(intent)
+                return true
+            }
+
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     fun updateUI(data:ArrayList<Pair<Double, Double>>){
